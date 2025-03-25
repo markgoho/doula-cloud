3fd6fd0b46f4e7dc12a7dbb79e3cde6d0dca3283
Mark Goho <mgoho@cisco.com>
2025-03-25 18:22:52 -0400

    update function rewrites

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,18 +1,6 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
 import { onRequest } from 'firebase-functions/v2/https';
 import express from 'express';
 import cors from 'cors';
-
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
 
 const app = express();
 app.use(cors({ origin: true }));
