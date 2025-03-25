e90687c3b9d77e2fcbfd7c020b51288d43f40d96
Mark Goho <mgoho@cisco.com>
2025-03-25 15:32:45 -0400

    working through functions

--- functions/eslint.config.mjs
+++ functions/eslint.config.mjs
@@ -4,7 +4,7 @@ import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 export default defineConfig([
-  globalIgnores(['eslint.config.mjs', '/lib/**/*', '/generated/**/*']),
+  globalIgnores(['eslint.config.mjs', '**/lib/**/*', '**/generated/**/*']),
   { files: ['**/*.{js,mjs,cjs,ts}'] },
   {
     files: ['**/*.{js,mjs,cjs,ts}'],
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,12 +8,23 @@
  */
 
 import { onRequest } from 'firebase-functions/v2/https';
-import * as logger from 'firebase-functions/logger';
+import express from 'express';
+import cors from 'cors';
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = onRequest((request, response) => {
-  logger.info('Hello logs!', { structuredData: true });
-  response.send('Hello from Firebase!');
+const app = express();
+app.use(cors({ origin: true }));
+
+app.get('/hello', (req, res) => {
+  res.send('Hello World!');
 });
+
+app.get('/:shortCode', (req, res) => {
+  res.send(
+    `Hello from the redirect endpoint! Short code: ${req.params.shortCode}`
+  );
+});
+
+export const api = onRequest(app);
